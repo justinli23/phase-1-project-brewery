060a64f3c2d65c20d800d64a195166b72d6e00c0
Justin Li <justinlit23@gmail.com>
2022-04-19 15:49:50 -0700

    day2

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,31 @@
-fetch("https://api.openbrewerydb.org/breweries")
-.then(resp => resp.json())
-.then(data => console.log(data))
+document.addEventListener("DOMContentLoaded", 
+    fetch("https://api.openbrewerydb.org/breweries")
+    .then(resp => resp.json())
+    .then(data => {
+    addBrewery()
+    addSearch()
+
+}))
+//Search by type, state, name.
+
+//Selectors
+const carousel = document.querySelector("#carousel")
+const form = document.querySelector("form");
+const searchResults = document.querySelector("#search-results")
+const favoritedResults = document.querySelector("#favorited-results")
+
+function addBrewery(brewery) {
+    const brewery = document.createElement("div")
+    const breweryName = brewery.name
+    const breweryType = brewery.Brewery_type
+    const breweryState = brewery.state
+    const brewerySite = brewery.Website_url
+    const likeButton = document.createElement("button")
+    brewery.append(likeButton)
+
+}
+
+//adds search functionality
+function addSearch () {
+    //looks through database using filter, finds matching values and appends them to search results
+}
